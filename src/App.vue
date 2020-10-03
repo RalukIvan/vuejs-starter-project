60b3eec2cbd0334bb7df4a98892517af955ef67c
RalukIvan <raluca.ivan87@gmail.com>
2020-10-03 16:00:27 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+ #app
+    Home(msg="Welcome to my vuejs-starter-project")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from '@/views/Home.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Home,
+    },
 }
 </script>
 
